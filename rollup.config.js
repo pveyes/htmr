7a3bcfd5c540417313b2602804bffade1f3efc13
Fatih Kalifa <fatihkalifa@gmail.com>
2017-08-13 17:27:39 +0700

    Improve browser bundle size to 1kB
    Use native browser HTML parser, and add bundlesize test

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,46 @@
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
-  entry: 'src/index.js',
-  format: 'cjs',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
-  external: ['posthtml-parser', 'react'],
-  dest: 'lib/index.js',
-};
+export default [
+  // cjs
+  {
+    entry: 'src/server.js',
+    format: 'cjs',
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+    external: ['posthtml-parser', 'react'],
+    dest: 'lib/index.js',
+  },
+  // umd
+  {
+    entry: 'src/browser.js',
+    format: 'iife',
+    globals: {
+      react: 'React',
+    },
+    plugins: [
+      json(),
+      resolve({
+        jsnext: false,
+        main: true,
+        browser: true,
+      }),
+      commonjs({
+        ignoreGlobal: true,
+        include: 'node_modules/**',
+      }),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      uglify(),
+    ],
+    external: ['react'],
+    dest: 'lib/htmr.umd.min.js',
+  },
+];
