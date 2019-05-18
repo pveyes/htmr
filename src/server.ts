01caec3ac95b2156e61642f92a5a52d19bc5de94
Fatih Kalifa <fatihkalifa@gmail.com>
2019-05-19 01:28:11 +0700

    Rewrite server parser to htmlparser2

--- src/server.ts
+++ src/server.ts
@@ -1,99 +1,91 @@
-// @flow
-import parse from 'posthtml-parser';
 import React, { ReactNode } from 'react';
+import { HtmrOptions, HTMLTags } from "./types";
+import Parser from 'htmlparser2';
 import { AllHtmlEntities as HtmlEntity } from 'html-entities';
 import mapAttribute from './mapAttribute';
 
-import { HtmrOptions, HTMLTags } from './types';
+type HTMLNode = {
+  type: 'tag' | 'style',
+  name: HTMLTags,
+  attribs: {
+    [key: string]: any
+  }
+  children: Array<Node>
+}
 
-// eslint-disable-next-line no-use-before-define
-type Node = ElementNode | TextNode;
+type TextNode = {
+  type: 'text',
+  data: string,
+  parent?: HTMLNode
+}
 
-type TextNode = string;
-
-type ElementNode = {
-  tag: HTMLTags,
-  attrs: {
-    [key: string]: string,
-  },
-  content: Array<Node>,
-};
+type Node = HTMLNode | TextNode;
 
 const TABLE_ELEMENTS = ['table', 'tbody', 'thead', 'tfoot', 'tr'];
 
 function transform(node: Node, key: string, options: HtmrOptions): ReactNode {
   const defaultTransform = options.transform._;
 
-  if (typeof node === 'string') {
-    // newline and space will be parsed as 'node' in posthtml-parser,
-    // we can ignore it along with comment node
-    const text = node.trim();
-    if (/^<!--[\s\S]+-->/.test(text)) {
-      return null;
+  switch (node.type) {
+    case 'style':
+    case 'tag': {
+      const { name, attribs } = node;
+
+      // decode all attribute value
+      Object.keys(attribs).forEach(key => {
+        attribs[key] = HtmlEntity.decode(attribs[key]);
+      });
+
+      const props = Object.assign(
+        {},
+        mapAttribute(attribs, options.preserveAttributes),
+        { key }
+      );
+
+      // if the tags children should be set dangerously
+      if (options.dangerouslySetChildren.indexOf(name) > -1) {
+        const childNode = <TextNode>node.children[0];
+        props.dangerouslySetInnerHTML = {
+          __html: childNode.data.trim()
+        };
+        return React.createElement(name, props, null);
+      }
+
+      const childNodes = node.children
+        .map((node, index) => transform(node, index.toString(), options))
+        .filter(Boolean);
+
+      // self closing component doesn't have children
+      const children = childNodes.length === 0
+        ? null
+        : childNodes;
+
+      const customElement = options.transform[name];
+      if (customElement) {
+        return React.createElement(customElement, props, children);
+      }
+
+      if (defaultTransform) {
+        return defaultTransform(name, props, children);
+      }
+
+      return React.createElement(name, props, children);
     }
+    case 'text': {
+      let str = node.data;
 
-    const str = HtmlEntity.decode(node);
-    return defaultTransform ? defaultTransform(str) : str;
+      if (node.parent && TABLE_ELEMENTS.indexOf(node.parent.name) > -1) {
+        str = str.trim();
+        if (str === '') {
+          return null;
+        }
+      }
+
+      str = HtmlEntity.decode(str);
+
+      return defaultTransform ? defaultTransform(str) : str;
+    }
   }
-
-  const { tag, attrs, content } = node;
-  const customElement = options.transform[tag];
-
-  // decode all attribute value
-  if (attrs) {
-    Object.keys(attrs).forEach(key => {
-      attrs[key] = HtmlEntity.decode(attrs[key]);
-    });
-  }
-
-  const props = Object.assign(
-    {},
-    mapAttribute(attrs, options.preserveAttributes),
-    // always set key because it's possible the html source contains
-    // multiple elements
-    { key }
-  );
-
-  // if the tags children should be set dangerously
-  if (options.dangerouslySetChildren.indexOf(tag) > -1) {
-    const innerHTML = <TextNode>content[0];
-    props.dangerouslySetInnerHTML = {
-      __html: innerHTML.trim()
-    };
-    return React.createElement(tag, props, null);
-  }
-
-  // self closing component doesn't have children
-  let children =
-    content === undefined
-      ? null
-      : content
-        .map((child, index) => {
-          if (TABLE_ELEMENTS.indexOf(tag) > -1 && typeof child == 'string') {
-            child = child.trim();
-            if (child === '') {
-              return null;
-            }
-          }
-
-          const childKey = `${key}.${index}`;
-          return transform(child, childKey, options);
-        })
-        .filter(child => child !== null);
-
-  if (children && children.length === 0) {
-    children = null;
-  }
-
-  if (customElement) {
-    return React.createElement(customElement, props, children);
-  }
-
-  if (defaultTransform) {
-    return defaultTransform(tag, props, children);
-  }
-
-  return React.createElement(tag, props, children);
 }
 
 export default function convertServer(html: string, options: Partial<HtmrOptions> = {}) {
@@ -106,11 +98,9 @@ export default function convertServer(html: string, options: Partial<HtmrOptions
     preserveAttributes: options.preserveAttributes || [],
     dangerouslySetChildren: options.dangerouslySetChildren || ["style"],
   };
-  const ast: Array<Node> = parse(html.trim());
 
-  const components = ast
-    .map((node, index) => transform(node, index.toString(), opts))
-    .filter(node => node !== null);
+  const ast = Parser.parseDOM(html.trim(), {}) as Array<HTMLNode>;
+  const components = ast.map((node, index) => transform(node, index.toString(), opts));
 
   if (components.length > 1) {
     return components;
